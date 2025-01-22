2479707e6a553bc2b051f61dce4d172b91dbab4f
user_11_20P1 <20p1_11@mail.ru>
2025-01-22 11:39:40 +0500

    Added sign in and sign up logic

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,144 @@
+const {PrismaClient} = require('@prisma/client');
+const argon = require('argon2');
+const signInStudent = async (req, res)=>{
+    const {login, password} = req.body;
+    const client = new PrismaClient();
+    let user = await client.student.findFirst({
+        where:{
+            login_student: login
+        },
+        select:{
+            name_student: true,
+            surname_student: true,
+            password_student: true,
+            patronymic_student: true,
+            id_class: true,
+            group:{
+                select:{
+                    title_class: true
+                }
+            }
+
+        }
+    });
+
+    const isPassValid = await argon.verify(user.password_student, password);
+    if (!isPassValid){
+        res.status(401).json({message: "Доступ запрещен"});
+    }
+    res.json(user);
+}
+
+const signInTeacher = async(req, res)=>{
+    const {login, password} = req.body;
+    const client = new PrismaClient();
+    let user = await client.teacher.findFirst({
+        where:{
+            login_teacher: login,
+        },
+        select:{
+            password_teacher: true,
+            name_teacher: true,
+            surname_teacher: true,
+            patronymic_teacher: true,
+            phone_teacher: true,
+            id_subject: true,
+            telegram_teacher: true,
+            email_teacher: true,
+            subject: {
+                select: {
+                    title_subject: true
+                }
+                
+            }
+        }
+    });
+    const isPassValid = await argon.verify(user.password_teacher, password);
+    if (!isPassValid){
+        res.status(401).json({message: "Доступ запрещен"});
+    }
+    res.json(user);
+}
+
+const signUpTeacher = async (req, res)=>{
+    const {login, password, name, surname, patronymic, phone, email, telegram, idSubject} = req.body;
+    const client = new PrismaClient();
+    const hashedPass = await argon.hash(password);
+    try{
+        const user = await client.teacher.create({
+            data:{
+                login_teacher: login,
+                password_teacher: hashedPass,
+                name_teacher: name,
+                surname_teacher: surname,
+                patronymic_teacher: patronymic,
+                phone_teacher: phone,
+                telegram_teacher: telegram,
+                email_teacher: email,
+                id_subject: idSubject
+            },
+            select:{
+                name_teacher: true,
+                surname_teacher: true,
+                patronymic_teacher: true,
+                phone_teacher: true,
+                id_subject: true,
+                telegram_teacher: true,
+                email_teacher: true,
+                subject: {
+                    select: {
+                        title_subject: true
+                    }
+                    
+                }
+            },
+        });
+        res.json(user);
+    }
+    catch(ex){
+        res.status(500).json({message: "Что-то пошло не так. Скорее всего такой логин уже существует."});
+    }
+    
+}
+
+const signUpStudent = async (req, res)=>{
+    const {login, password, name, surname, patronymic, classId} = req.body;
+    const client = new PrismaClient();
+    const hashedPass = await argon.hash(password);
+    try{
+        const user = await client.student.create({
+            data:{
+                login_student: login,
+                password_student: hashedPass,
+                name_student: name,
+                surname_student: surname,
+                patronymic_student: patronymic,
+                id_class: classId
+            },
+            select:{
+                name_student: true,
+                surname_student: true,
+                patronymic_student: true,
+                id_class: true,
+                group:{
+                    select:{
+                        title_class: true
+                    }
+                }
+            }
+        });
+        res.json(user);
+    }
+    catch(ex){
+        res.status(500).json({message: "Что-то пошло не так. Скорее всего такой логин уже существует.", error: ex});
+    }
+    
+
+}
+
+module.exports = {
+    signInStudent,
+    signInTeacher,
+    signUpTeacher,
+    signUpStudent
+}
